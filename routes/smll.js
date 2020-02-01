1cb134f5f1a1cf59991ef9ed1b6ac330e13ed1dd
Fabrício Yamamoto <fabriciorby@hotmail.com>
2020-02-01 17:52:54 -0300

    API funcional

--- routes/smll.js
+++ routes/smll.js
@@ -1,17 +1,35 @@
 const router = require('express-promise-router')();
-const { alphaVantageAPI, timeRange } = require('../api/alpha-vantage-api')
 const loadDB = require('../db');
 
-router.get('/semanal', async (req, res, next) => {
+const getCollection = async () => {
     const db = await loadDB();
-    const col = await db.collection('smll_cotacoes');
-    const data = await col.find({}, {_id: 0}).toArray();
+    return await db.collection('smll_cotacoes');
+}
+
+router.get('/weekly/all', async (req, res, next) => {
+    const col = await getCollection();
+    const data = await col.find({}).project({ _id: 0 }).toArray();
     res.status(200).send(data);
 });
 
-router.get('/diario', async (req, res, next) => {
-    const data = await alphaVantageAPI(timeRange.daily, 'GOLL4.SA');
+router.get('/weekly/:ticker', async (req, res, next) => {
+    const col = await getCollection();
+    const data = await col.findOne({ papel: `${req.params.ticker}.SA` }, { projection: { _id: 0 } });
     res.status(200).send(data);
 });
+
+router.get('/all', async (req, res, next) => {
+    const db = await loadDB();
+    const col = await db.collection('smll_index');
+    const data = await col.find({}).project({ _id: 0 }).toArray();
+    res.status(200).send(data);
+})
+
+router.get('/all/code', async (req, res, next) => {
+    const db = await loadDB();
+    const col = await db.collection('smll_index');
+    const data = await col.find({}).project({ _id: 0, nome: 0 }).toArray().map(item => item.codigo).sort();
+    res.status(200).send(data);
+})
 
 module.exports = router;
