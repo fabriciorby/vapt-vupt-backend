f88fdf2d5177801fdce3dd690d46c6d350c2c20d
Fabrício Yamamoto <fabriciorby@hotmail.com>
2020-02-02 15:39:45 -0300

    add scheduler e args para syncar db

---
+++ scheduler.js
@@ -0,0 +1,26 @@
+const schedule = require('node-schedule');
+
+const scheduleSyncDB = () => {
+    schedule.scheduleJob({ hour: 0, minute: 0, dayOfWeek: 6 }, async () => {
+        console.log('Scheduler starting...');
+        let isSynced = false;
+        do {
+            console.log(new Date().toLocaleString);
+            console.log('Trying to sync...');
+            isSynced = await doSyncSmallCapsIntoDB();
+        } while (!isSynced)
+    });
+}
+
+const myScheduler = {
+    start: () => {
+        scheduleSyncDB();
+        console.log('Scheduler initialized.')
+    },
+    syncNow: () => {
+        // doSyncSmallCapsIntoDB();
+        console.log('sync agora men');
+    }
+}
+
+module.exports = myScheduler;
--- syncDb.js
+++ syncDb.js
@@ -19,7 +19,7 @@ const getSmallCapsIndexFromDB = async () => {
 
 const getSmallCapsIndexAndSync = async () => {
     let smallCapsIndex;
-    console.log(`Getting SMLL Index from BVMF website...`)
+    console.log(`Getting SMLL Index from BVMF website...`);
     let table = await convertUrl(smallCapsIndexURL);
     table = table[0];
     if (table) {
@@ -27,19 +27,19 @@ const getSmallCapsIndexAndSync = async () => {
         smallCapsIndex = table.map(toSmallCapIndexSchema);
         await syncSmallCapsIndexIntoDB(smallCapsIndex);
     } else {
-        console.log(`BVMF website is down.`)
+        console.log(`BVMF website is down.`);
         smallCapsIndex = await getSmallCapsIndexFromDB();
     }
     return smallCapsIndex;
 }
 
 const syncSmallCapsIndexIntoDB = async (smallCapsIndex) => {
-    console.log("Syncing SMLL Index into DB...")
+    console.log("Syncing SMLL Index into DB...");
     const db = await loadDB();
     const col = await db.collection('smll_index');
     const syncSmallCaps = smallCapsIndex.map(async stock => {
-        const doSync = await col.replaceOne({ codigo: stock.codigo }, stock, { upsert: true })
-        if (doSync.upsertedId) console.log(`${stock.codigo}: New stock entry created.`)
+        const doSync = await col.replaceOne({ codigo: stock.codigo }, stock, { upsert: true });
+        if (doSync.upsertedId) console.log(`${stock.codigo}: New stock entry created.`);
     });
     await Promise.all(syncSmallCaps);
     return true;
@@ -79,10 +79,10 @@ const syncStockPricesIntoDB = async (stocksPrices) => {
 
 const removeItemFromList = (item, list) => list.splice(list.indexOf(item), 1);
 
-(async () => {
+const doSyncSmallCapsIntoDB = async () => {
     const smallCapsIndex = await getSmallCapsIndexAndSync();
     let stocksMissingPrice = [...smallCapsIndex].map(e => e.codigo + '.SA');
-    console.log('Getting stock prices and syncing into DB.')
+    console.log('Getting stock prices and syncing into DB.');
     while (stocksMissingPrice.length != 0) {
         const stocksPrices = await getStockPricesAndUpdate(stocksMissingPrice);
         await syncStockPricesIntoDB(stocksPrices);
@@ -91,5 +91,8 @@ const removeItemFromList = (item, list) => list.splice(list.indexOf(item), 1);
         console.log('One more time. 🎵');
         await sleep(31000);
     }
-    console.log('Synced.')
-})();
+    console.log('Synced.');
+    return true;
+}
+
+module.exports = doSyncSmallCapsIntoDB;
